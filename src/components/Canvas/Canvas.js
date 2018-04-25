5ae549ca957b9aeedb3037b1b45aa472255b3ba9
Tom Bran <tom.bran@gmail.com>
2018-04-25 13:15:17 +0100

    Fix scrollbars - use auto-height (via ghost element)

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 import debounce from 'lodash.debounce';
 import { URL_UPDATE_DELAY } from 'config';
 import { setUrlParams } from 'utils/url';
-import { Wrapper, Text } from './Canvas.styles';
+import { Wrapper, Text, Ghost } from './Canvas.styles';
+
+const DEFAULT_HEIGHT = 100;
 
 export class Canvas extends Component {
 	static propTypes = {
@@ -11,20 +13,51 @@ export class Canvas extends Component {
 		changeText: PropTypes.func.isRequired,
 	};
 
+	state = {
+		height: DEFAULT_HEIGHT,
+	};
+
 	constructor(props) {
 		super(props);
+
+		this.ghostRef = React.createRef();
 
 		this.updateUrlDebounced = debounce(this.updateUrl, URL_UPDATE_DELAY);
 	}
 
+	componentDidMount() {
+		this.mounted = true;
+
+		this.setFilledTextareaHeight();
+	}
+
+	setFilledTextareaHeight() {
+		if (this.mounted) {
+			this.setState({
+				height: this.ghostRef.current.scrollHeight,
+			});
+		}
+	}
+
 	render() {
 		const { text } = this.props;
+		const { height } = this.state;
+
+		const isOneLine = height <= DEFAULT_HEIGHT;
 
 		return (
 			<Wrapper>
+				<Ghost
+					aria-hidden={true}
+					innerRef={this.ghostRef}
+					value={text}
+					readOnly={true}
+				/>
 				<Text
+					style={{ height, resize: isOneLine ? 'none' : null }}
 					onChange={this.handleTextChange}
-					onKeyUp={this.updateUrlDebounced}
+					onKeyUp={this.handleKeyUp}
+					autoFocus={true}
 					value={text}
 					placeholder="Type something…"
 				/>
@@ -33,6 +66,11 @@ export class Canvas extends Component {
 	}
 
 	handleTextChange = event => this.props.changeText(event.target.value);
+
+	handleKeyUp = () => {
+		this.updateUrlDebounced();
+		this.setFilledTextareaHeight();
+	};
 
 	updateUrl = () => {
 		const { text } = this.props;
