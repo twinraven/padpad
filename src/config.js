b9bc9c7f6b0151a0302d76a4f4d5cd2b02cbf3af
Tom Bran <tom.bran@gmail.com>
2018-05-04 16:22:07 +0100

    Update config

--- src/config.js
+++ src/config.js
@@ -16,7 +16,8 @@ export const DEFAULT_PARAMS = {
 	text: DEFAULT_TEXT,
 };
 
-export const URL_UPDATE_DELAY = 350;
+export const URL_UPDATE_DELAY = 250;
+export const COLOR_UPDATE_DELAY = 100;
 export const RESIZE_UPDATE_DELAY = 100;
 
 export const MIN_FONT_SIZE = 10;
