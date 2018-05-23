4bf4553f6a46f5df35b7b193bd87f63582aa38ce
Tom Bran <email@tombran.co.uk>
2018-05-23 09:37:29 +0100

    fix title calculation - ignore <br> too

--- src/app/App.utils.js
+++ src/app/App.utils.js
@@ -4,7 +4,7 @@ import { DEFAULT_TITLE } from 'config.js';
 export function getTitle(text) {
 	if (isUndefined(text) || text.length === 0) return DEFAULT_TITLE;
 
-	const firstLine = text.split('\n')[0];
+	const firstLine = text.split(/(\\n|<br ?\/?>)/)[0];
 	let title = firstLine.substring(0, 25);
 
 	if (title.length < firstLine.length) {
